b2371253fd3501a69b22616c374de5abd2f2c5e9
anusha1466 <anusharajannaanusha@gmail.com>
2026-04-11 19:28:49 +0530

    Create Stack.c

---
+++ Stack.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#define MAX 5
+
+int stack[MAX];
+int top = -1;
+
+// Push operation
+void push() {
+    int value;
+    if (top == MAX - 1) {
+        printf("Stack Overflow\n");
+    } else {
+        printf("Enter value to push: ");
+        scanf("%d", &value);
+        top++;
+        stack[top] = value;
+        printf("Inserted %d\n", value);
+    }
+}
+
+// Pop operation
+void pop() {
+    if (top == -1) {
+        printf("Stack Underflow\n");
+    } else {
+        printf("Deleted %d\n", stack[top]);
+        top--;
+    }
+}
+
+// Display stack
+void display() {
+    if (top == -1) {
+        printf("Stack is empty\n");
+    } else {
+        printf("Stack elements:\n");
+        for (int i = top; i >= 0; i--) {
+            printf("%d\n", stack[i]);
+        }
+    }
+}
+
+int main() {
+    int choice;
+
+    while (1) {
+        printf("\n--- STACK MENU ---\n");
+        printf("1. Push\n2. Pop\n3. Display\n4. Exit\n");
+        printf("Enter choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1: push(); break;
+            case 2: pop(); break;
+            case 3: display(); break;
+            case 4: return 0;
+            default: printf("Invalid choice\n");
+        }
+    }
+}
